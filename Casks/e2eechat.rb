dd989d591f7b25c8fd9f99a62a8c20852188de75
ekaterinburg <82260201+ekaterinburg001@users.noreply.github.com>
2026-07-29 23:19:43 +0800

    Update E2EEChat cask to 2.0.4.11

--- Casks/e2eechat.rb
+++ Casks/e2eechat.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 cask "e2eechat" do
-  version "2.0.3.10"
-  sha256 "e67cb1837714bbeb8366cd92049c715212f462a5c174172cab56be1ef73b2ea2"
+  version "2.0.4.11"
+  sha256 "8da2797fa00eca47420e08bf88f1e20ef90fa16bab15d522fcbef1d867cf06e6"
 
   url "https://github.com/ekaterinburg001/homebrew-e2eechat/releases/download/v#{version}/E2EEChat-1-Bundled-Whisper-Universal.zip"
   name "E2EEChat"
