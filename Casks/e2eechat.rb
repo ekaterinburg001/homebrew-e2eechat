6360d8dfdde6d86ccfa5583f750b2884216fcc67
tianyi <tianyi@Ekaterinburgs-MacBook-Air.local>
2026-08-03 22:46:43 +0800

    Update E2EEChat to 2.0.4.19

--- Casks/e2eechat.rb
+++ Casks/e2eechat.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 cask "e2eechat" do
-  version "2.0.4.18"
-  sha256 "40875eb929e42ee3877ce80e3eb1f5fd96100b281b864df37ffb36731f615487"
+  version "2.0.4.19"
+  sha256 "5dc239c3fa49132bb9fef8a84cf2a586155ee0e24ad7c18ed682f60e87965a58"
 
   url "https://github.com/ekaterinburg001/homebrew-e2eechat/releases/download/v#{version}/E2EEChat-1-Bundled-Whisper-Universal.zip"
   name "E2EEChat"
